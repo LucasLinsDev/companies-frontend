7f3d27aae246dff605cb7e204f2ea1fe705de65b
LucasLinsDev <lucaslinsperfil@gmail.com>
2023-09-11 02:07:54 -0300

    feat(eslint) configuracao de vue/multi-word off

---
+++ src/components/Ui/Logo.vue
@@ -0,0 +1,7 @@
+<template>
+  <q-page>
+    <h1>lorem</h1>
+  </q-page>
+</template>
+<script setup>
+</script>
--- src/pages/AuthPage.vue
+++ src/pages/AuthPage.vue
@@ -1,7 +1,8 @@
 <template>
   <q-page padding class="bg-primary">
-
+    <Logo/>
   </q-page>
 </template>
 <script setup>
+import Logo from 'src/components/Ui/Logo.vue'
 </script>
